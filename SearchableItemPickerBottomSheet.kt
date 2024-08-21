f2d311b3cf6336b148ae19408741e5f7670cb767
Salam El-Banna <9810106+SecretA@users.noreply.github.com>
2024-08-22 00:58:43 +0300

    Update SearchableItemPickerBottomSheet.kt

--- SearchableItemPickerBottomSheet.kt
+++ SearchableItemPickerBottomSheet.kt
@@ -35,7 +35,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.real.realgpt.R
 
 
 /**
